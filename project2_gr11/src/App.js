7044216a3dfd3a3816e4c0a6455e5ffbac2c6182
Minh Ha Do <minhhd@stud.ntnu.no>
2018-09-18 15:36:41 +0200

    generated some radio buttons

--- project2_gr11/src/App.js
+++ project2_gr11/src/App.js
@@ -1,21 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import { CallAJAXTextMedia } from "./components/CallAJAXTextMedia";
+import { GenerateRadioBtn } from "./components/GenerateRadioBtn";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+
+        render()
+        {
+            return (
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <h1 className="App-title">Welcome to React</h1>
+                    </header>
+                    <p className="App-intro">
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    {/*AJAX test render of text title*/}
+                    <div className="textContainer">
+                        {/*<CallAJAXTextMedia/>*/}
+                    </div>
+
+                    {/*RADIOBUTTON TEST*/}
+                    <GenerateRadioBtn />
+                </div>
+            );
+        }
+    }
+
 
 export default App;
