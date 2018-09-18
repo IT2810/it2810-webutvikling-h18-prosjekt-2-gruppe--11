b2170364318a62b33bf936bab945786d7e642a7c
kimdu <kimdu@stud.ntnu.no>
2018-09-18 12:48:49 +0200

    Pushed branch up

--- project2_gr11/src/App.js
+++ project2_gr11/src/App.js
@@ -1,19 +1,21 @@
+// Base of the entire site (The presentation)
+
+// Libraries
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <nav>
+            <ul>hei</ul>
+            <ul>hallo</ul>
+            <ul>hey</ul>
+            <ul>yo</ul>
+        </nav>
     );
   }
 }
