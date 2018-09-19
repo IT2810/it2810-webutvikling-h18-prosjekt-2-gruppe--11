29456fc076d0567765d84253c15f20196367bc3d
Minh Ha Do <minhhd@stud.ntnu.no>
2018-09-19 19:49:38 +0200

    fixed name #10

--- project2_gr11/src/App.js
+++ project2_gr11/src/App.js
@@ -35,15 +35,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-
-                <div dangerouslySetInnerHTML={{__html: this.state.texts}} />;
+                <div dangerouslySetInnerHTML={{__html: this.state.texts}} />
 
                 <div className="textContainer">
                 </div>
@@ -60,7 +52,7 @@ class App extends Component {
 
                 <div>
                     <audio controls>
-                        <source src="sound_media/surpriseSounds/1.mp3" type="audio/mpeg" />
+                        <source src="soundMedia/surpriseSounds/1.mp3" type="audio/mpeg" />
                         Your browser does not support the audio element.
                     </audio>
                 </div>
