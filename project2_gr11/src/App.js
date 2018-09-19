6a25d0aa0886fc069f870a5a010f40f7e483c5dd
CatrionaTorklep <catriona.torklep@gmail.com>
2018-09-19 13:19:54 +0200

    Made it possible to load SVG images

--- project2_gr11/src/App.js
+++ project2_gr11/src/App.js
@@ -3,19 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            texts: ''
+        };
+    }
+
+   /* getSVG() { return {__html: 'First &middot; Second'}; };
+<div dangerouslySetInnerHTML={getSVG()} /> */
+
+    componentDidMount() {
+        let XMLParser = require('react-xml-parser');
+        fetch('imageMedia/plainCats/plainbrowncat.svg')
+            .then(response => response.text())
+            .then(svg => {
+                return this.setState({
+                    texts: svg
+                });
+            }
+            )
+        }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <div dangerouslySetInnerHTML={{__html: this.state.texts}} />;
+            </div>
+        )
 }
+}
+
 
 export default App;
